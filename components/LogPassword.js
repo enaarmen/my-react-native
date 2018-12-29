0744503615ce0305dab6c2b460e0fe4335d16e37
yves adonias <yves.adonias@epitech.eu>
2018-12-29 17:21:21 +0100

    push de version écrasée.

--- components/LogPassword.js
+++ components/LogPassword.js
@@ -22,50 +22,65 @@ export default class LogPassword extends Component {
     constructor(props) {
         super(props);
         this.pass = '';
-        this.passAuth = false;
+        this.pressed = false;
+        this.firsttime = true;
+        this.state = this.props.store;
     }
 
     onLoginButtonPress(username, password) {
-        this.passAuth = true;
-        this.props.store = this.setState({
+        this.props.store = {
           user: {
             loggedIn: true,
             username: username,
             password: password
           }
-        });
+        }
+        this.setState(this.props.store);
+        this.pressed = true;
+        //this.firsttime = false;
+        return (this.state);
       }
+    
+    /*renderLog (username, password) {
+        this.onLoginButtonPress(username, password);
+        return (() => <LoggedIn store={this.props.store} />);
+    }*/
+    myAddPass(username, password) {
+        this.firsttime = false;
+        this.setState({ user: { loggedIn: false, username: username, password: password}})
+        return (this.state);
+    }
 
     render() {
-        if (!this.passAuth) {
+        console.log("MY state:", this.state);
+        /*if (this.state != null && this.pressed && this.state.user.loggedIn)
+            return (
+                <LoggedIn store={this.state} />
+            );*/
+        /*else*/
+        if (this.pressed) { //this.state.user.loggedIn)
+            return (
+                <View><LoggedIn store={this.state} /></View>
+            );
+        } else if (this.firsttime && this.pass == '') {
             return (
                 <View>
                     <Text>Enter your password.</Text>
-                    <TextInput onChangeText={(text) => this.pass = text} onEndEditing={() => {this.onLoginButtonPress(this.props.store.user.username, this.pass)}} />
+                    <TextInput onChangeText={(text) => this.pass = text} onEndEditing={() => this.myAddPass(this.props.store.user.username, this.pass)} />
                 </View>
             );
-        } else {
-            return (
-                <View>
-                    <Button title="connect" onPress={() => {return (<LoggedIn store={this.state} />);}} />
-                </View>
-            );
-        }
+        } else if (!this.props.store.user.loggedIn && !this.pressed)
+            return (<View><Button title="connect" onPress={() => this.onLoginButtonPress(this.state.username, this.state.user.password)} /></View>);
+        else
+            return(<View><Text>Click on logging button</Text><Button title="connect" onPress={() => this.onLoginButtonPress(this.props.store.user.username, this.pass)} /></View>);
+        /*else if ('user' in this.state && this.state.user.loggedIn && this.state.user.password != '') {
+            <Button title="connect" onPress={() => {this.onLoginButtonPress(this.state.user.username, this.state.user.password); return (<LoggedIn store={this.state} />)}} />
+        } */
+        /*if (this.state.user.loggedIn)
+            return (<View><LoggedIn store={this.state} /></View>);*/
     }
-         
-        /*if (!this.state || ('user' in this.state && !this.state.user.loggedIn)) {
-            return (<View>
-                <Text>Enter your password.</Text>
-                <TextInput onChangeText={(text) => this.pass = text} onEndEditing={() => this.onLoginButtonPress(this.props.store.user.username, this.pass)} />
-            </View>
-            );
-        } else if ('user' in this.state && this.state.user.loggedIn) {
-            return (
-                <Button title="connect" onPress={() => {return (<View><LoggedIn store={this.state} /></View>)}} />
-            );
-        } else {
-            return (<View><Text>You are out of context.</Text></View>);
-        }*/
 }
+
+////this.setState({ user: { loggedIn: true, username: this.props.store.user.username, password: this.pass}})} />
 
 connect(mapStateToProps, mapDispatchToProps)(LoggedIn);
